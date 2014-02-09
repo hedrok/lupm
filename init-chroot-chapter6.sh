788e2ffbf904c23d2ba50227d8ff3d9b98370055
Kirill Yatsenko <kirill.yatsenko@hedrok.org>
2014-02-09 18:43:16 +0200

    Add script for init chroot after chapter6
    When /tools is deleted, init-chroot stops working.
    Move it to separate script init-chroot-chapter6.sh
    and change init-chroot.sh accordingly

--- init-chroot-chapter6.sh
+++ init-chroot-chapter6.sh
@@ -3,10 +3,10 @@ set -e
 
 export LFS=/mnt/lfs
 
-chroot "$LFS" /usr/bin/env -i \
+chroot "$LFS" /tools/bin/env -i \
    HOME=/root                  \
    TERM="$TERM"                \
    MAKEFLAGS='-j 6'            \
    PS1='\u:\w\$ '              \
-   PATH=/usr/src/scripts/lupm/:/bin:/usr/bin:/sbin:/usr/sbin \
-   /bin/bash --login +h
+   PATH=/usr/src/scripts/lupm/:/bin:/usr/bin:/sbin:/usr/sbin:/tools/bin \
+   /tools/bin/bash --login +h
