0c287980342e3f24d778d55a83caa080854e7b96
Kirill Yatsenko <kirill.yatsenko@hedrok.org>
2014-02-26 21:03:19 +0200

    Xorg applications

--- scripts/xorg/xorg-init.sh
+++ scripts/xorg/xorg-init.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
+PREFIX=/usr
 groupadd xorginstall;
-for d in X11/extensions X11/fonts X11/dri GL GL/internal xcb X11; do
-    mkdir -pv /usr/include/$d/;
-    lupm-dir.sh /usr/include/$d/ xorginstall;
+for d in \
+         include/X11/extensions \
+         include/X11/fonts \
+         include/X11/dri \
+         include/GL \
+         include/GL/internal \
+         include/xcb \
+         include/X11 \
+         share/X11 \
+         share/X11/app-defaults \
+         ; do
+    mkdir -pv $PREFIX/$d/;
+    lupm-dir.sh $PREFIX/$d/ xorginstall;
 done;
+#forbid installing locale files
+mkdir -pv $PREFIX/share/X11/locale
