bb0033365e2a91ad360a43b1abf9bb2838994470
Kirill Yatsenko <kirill.yatsenko@hedrok.org>
2016-03-13 08:32:54 +0200

    Add packages to lupm-dirs

--- lupm/lupm-create-hierarchy.sh
+++ lupm/lupm-create-hierarchy.sh
@@ -4,6 +4,7 @@ prefix=$1;
 group=$2;
 mkdir -pv $prefix/{bin,lib,include,share};
 mkdir -pv $prefix/share/{info,locale,doc,man};
+mkdir -pv $prefix/share/mime/packages
 mkdir -pv $prefix/share/man/man{1,2,3,4,5,6,7,8};
 case $(uname -m) in
     x86_64)
@@ -23,3 +24,4 @@ lupm-dir.sh $prefix/share/man/man5 $group;
 lupm-dir.sh $prefix/share/man/man6 $group;
 lupm-dir.sh $prefix/share/man/man7 $group;
 lupm-dir.sh $prefix/share/man/man8 $group;
+lupm-dir.sh $prefix/share/mime/packages $group;
